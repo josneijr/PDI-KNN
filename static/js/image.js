3e943927e682aa08d019cd46278c710db44d489a
Josnei Luis Olszewski Junior <josneijr@gmail.com>
2017-12-07 23:07:51 -0200

    Reorganizing things

--- static/js/image.js
+++ static/js/image.js
@@ -1,4 +1,4 @@
-function CalcImageHistogram(imagePath){
+function CalcImageHistogram(imageObj){
 
     var histR = Array.apply(null,  Array(256)).map(Number.prototype.valueOf,0);
     var histG = Array.apply(null,  Array(256)).map(Number.prototype.valueOf,0);
@@ -7,26 +7,23 @@ function CalcImageHistogram(imagePath){
     var canvas = document.createElement('canvas');
     var context = canvas.getContext("2d");
 
-    var imageObj = new Image();
-    imageObj.onload = function()
+    canvas.width = imageObj.width;
+    canvas.height = imageObj.height;
+    context.drawImage(imageObj, 0, 0);
+
+    var imageData = context.getImageData(0,0,imageObj.width,imageObj.height);
+    
+    var pix = imageData.data;
+
+    console.log(pix);
+
+    // Loop over each pixel
+    for (var i=0; i < pix.length; i += 4) 
     {
-        canvas.width = imageObj.width;
-        canvas.height = imageObj.height;
-        context.drawImage(imageObj, 0, 0);
-
-        var imageData = context.getImageData(0,0,imageObj.width,imageObj.height);
-        
-        var pix = imageData.data;
-
-        // Loop over each pixel
-        for (var i=0; i < pix.length; i += 4) 
-        {
-            histR[pix[i]  -1]++; // red
-            histG[pix[i+1]-1]++; // green
-            histB[pix[i+2]-1]++; // blue
-        }  
-    };
-    imageObj.src = imagePath;
+        histR[pix[i]  -1]++; // red
+        histG[pix[i+1]-1]++; // green
+        histB[pix[i+2]-1]++; // blue
+    }  
 
     var histogram = [];
 
