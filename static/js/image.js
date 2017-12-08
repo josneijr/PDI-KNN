7b6123891aab7c3a029be4d8e47b046e42764e46
Josnei Luis Olszewski Junior <josneijr@gmail.com>
2017-12-07 22:47:42 -0200

    All image operations are all set!

--- static/js/image.js
+++ static/js/image.js
@@ -1,8 +1,38 @@
-function CalcImageHistogram(imageArray){
+function CalcImageHistogram(imagePath){
 
-    var histRed = Array.apply(null,  Array(255)).map(Number.prototype.valueOf,0);
-    var histGreen = Array.apply(null,  Array(255)).map(Number.prototype.valueOf,0);
-    var histBlue = Array.apply(null,  Array(255)).map(Number.prototype.valueOf,0);
+    var histR = Array.apply(null,  Array(256)).map(Number.prototype.valueOf,0);
+    var histG = Array.apply(null,  Array(256)).map(Number.prototype.valueOf,0);
+    var histB = Array.apply(null,  Array(256)).map(Number.prototype.valueOf,0);
 
-    
+    var canvas = document.createElement('canvas');
+    var context = canvas.getContext("2d");
+
+    var imageObj = new Image();
+    imageObj.onload = function()
+    {
+        canvas.width = imageObj.width;
+        canvas.height = imageObj.height;
+        context.drawImage(imageObj, 0, 0);
+
+        var imageData = context.getImageData(0,0,imageObj.width,imageObj.height);
+        
+        var pix = imageData.data;
+
+        // Loop over each pixel
+        for (var i=0; i < pix.length; i += 4) 
+        {
+            histR[pix[i]  -1]++; // red
+            histG[pix[i+1]-1]++; // green
+            histB[pix[i+2]-1]++; // blue
+        }  
+    };
+    imageObj.src = imagePath;
+
+    var histogram = [];
+
+    histogram = histogram.concat(histR);
+    histogram = histogram.concat(histG);
+    histogram = histogram.concat(histB);
+
+    return histogram;
 }
